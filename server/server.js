e21a0dbc57727581a34c73cd41fe639c01b15d69
Diego Keller <keller.diego@gmail.com>
2017-07-03 16:48:39 -0300

    Add GET /users/me and auth.

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ const bodyParser = require('body-parser');
 var {mongoose} = require('./db/mongoose.js');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+var {authenticate} = require('./middleware/authenticate');
 
 const {ObjectID} = require('mongodb');
 const port = process.env.PORT;
@@ -123,6 +124,26 @@ app.post('/users', (req, res) => {
     console.log(e);
     res.status(400).send(e);
   });
+});
+
+var authenticate = (req, res, next) => {
+  var token = req.header('x-auth');
+
+  User.findByToken(token).then((user) => {
+      if(!user){
+        return Promise.reject();
+      }
+
+      req.user = user;
+      req.token = token;
+      next();
+  }).catch((e) => {
+    res.status(401).send();
+  });
+};
+
+app.get('/users/me', authenticate, (req, res) => {
+  res.send(req.user);
 });
 
 app.listen(port, () => {
